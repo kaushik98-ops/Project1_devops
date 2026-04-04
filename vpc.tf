6d677a9a205c260a69b038bd899b69018c6f6b96
kaushik98-ops <60217964+kaushik98-ops@users.noreply.github.com>
2026-04-04 12:44:29 +0530

    Change availability zones for subnets to us-east

--- vpc.tf
+++ vpc.tf
@@ -5,25 +5,25 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public1" {
     vpc_id= aws_vpc.main.id
     cidr_block= '10.0.1.0/24"
-    availability_zone= "ap-south-1a'
+    availability_zone= "us-east-1a'
 }
 
 resource "aws_subnet" "public2" {
     vpc_id= aws_vpc.main.id
     cidr_block= '10.0.2.0/24"
-    availability_zone= "ap-south-1b'
+    availability_zone= "us-east-1b'
 }
 
 resource "aws_subnet" "private1" {
     vpc_id= aws_vpc.main.id
     cidr_block= '10.0.3.0/24"
-    availability_zone= "ap-south-1a'
+    availability_zone= "us-east-1a'
 }
 
 resource "aws_subnet" "private2" {
     vpc_id= aws_vpc.main.id
     cidr_block= '10.0.4.0/24"
-    availability_zone= "ap-south-1b'
+    availability_zone= "us-east-1b'
 }
 
 resource "aws_internet_gateway" "igw"{
