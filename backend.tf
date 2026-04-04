5c81bcc1d4ec847a46bb3bc15aa2c9a0f45ed538
kaushik98-ops <60217964+kaushik98-ops@users.noreply.github.com>
2026-04-04 12:37:38 +0530

    Change Terraform region from ap-south-1 to us-east-1

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform{
     bucket= "my-terraform-state-bucket"
     key= "proj1/terraform.tfstate"
-    region= "ap-south-1"
+    region= "us-east-1"
     dynamodb_table= "my-terraform-lock-table"
 }
